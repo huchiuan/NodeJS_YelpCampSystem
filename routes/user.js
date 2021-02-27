2b87cf58632d81151dcefd26c9df9a4ad8ca41fd
huchiuan <tusna08124@gmail.com>
2021-02-27 23:59:18 +0800

    <將user 註冊的postFUN寫好 並可以註冊>

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,29 @@
 const express =require('express');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 
 router.get('/register',(req,res)=>{
     res.render('users/register') //導向EJS
 })
-router.post('/register',async(req,res)=>{
-    res.send(req.body);//{"username":"ss","email":"tusna08124@gmail.com","password":"ww"}
-})
+router.post('/register',catchAsync(async(req,res)=>{
+    //res.send(req.body);//{"username":"ss","email":"tusna08124@gmail.com","password":"ww"}
+    try{
+        const{ email,username,password}=req.body;
+        const user =new User ({email,username});
+        const registerdUser = await User.register(user,password);  //.register是passport的fun 可以把不用salt的放前面 要加salt的放後面
+        //也會用passport 判斷MONGODB 裡面的資料的狀況
+        console.log(registerdUser);
+        req.flash('success','註冊成功');
+        res.redirect('/campgrounds');
+    } catch(e){
+        req.flash('error',e.message);
+        res.redirect('register');
+    }
+    
+   
+}))
+
+
 module.exports =router;
