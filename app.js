0055fe942042c52927c91cec5e036fa127ce1878
huchiuan <tusna08124@gmail.com>
2021-02-28 00:50:27 +0800

    <使用passport的login fun 讓使用者註冊完 能直接以登入狀態進入頁面>

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req,res,next)=>{
+   res.locals.currentUser=req.user;
    res.locals.success=req.flash('success');
    res.locals.error =req.flash('error');  //丟到partials的flash處理
    next();
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 router.get('/register',(req,res)=>{
     res.render('users/register') //導向EJS
 })
-router.post('/register',catchAsync(async(req,res)=>{
+router.post('/register',catchAsync(async(req,res,next)=>{
     //res.send(req.body);//{"username":"ss","email":"tusna08124@gmail.com","password":"ww"}
     try{
         const{ email,username,password}=req.body;
@@ -16,8 +16,13 @@ router.post('/register',catchAsync(async(req,res)=>{
         const registerdUser = await User.register(user,password);  //.register是passport的fun 可以把不用salt的放前面 要加salt的放後面
         //也會用passport 判斷MONGODB 裡面的資料的狀況
         console.log(registerdUser);
-        req.flash('success','註冊成功');
-        res.redirect('/campgrounds');
+
+        req.login(registerdUser,err =>{ //passport 的login fun 在註冊完可以直接登入用 不用再自己登入一次
+            if (err) return next(err);
+            req.flash('success','註冊成功');
+            res.redirect('/campgrounds');
+        })
+       
     } catch(e){
         req.flash('error',e.message);
         res.redirect('register');
