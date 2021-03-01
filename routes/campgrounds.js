b9e26e67f53d688e6b1dc7c79a586a940ca9e58b
huchiuan <tusna08124@gmail.com>
2021-03-01 21:24:53 +0800

    <使用multer 跟 multer-storage-cloudinary 實作圖片上傳功能>

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,15 +9,17 @@ const{isLoggedIn,isAuthor,validateCampground} = require('../middleware');
 
 
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const{storage} = require('../cloudinary');
+const upload = multer({ storage});
 
 
 
 router.route('/')
         .get(catchAsync(campgrounds.index))
         //.post(isLoggedIn,validateCampground ,catchAsync(campgrounds.createCampground))
-        .post(upload.single('image'),(req,res)=>{
-            res.send(req.body,req.files)
+        .post(upload.array('image'),(req,res)=>{
+            console.log(req.body,req.files);
+            res.send('!')
         })
 
 
