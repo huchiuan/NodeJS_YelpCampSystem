4dc8930d5ea1ddd8dc8eea1606b8d3e4a9d24504
huchiuan <tusna08124@gmail.com>
2021-03-01 20:50:59 +0800

    <測試env>

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,11 +8,17 @@ const Campground=require('../models/campground');
 const{isLoggedIn,isAuthor,validateCampground} = require('../middleware');
 
 
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
 
 router.route('/')
         .get(catchAsync(campgrounds.index))
-        .post(isLoggedIn,validateCampground ,catchAsync(campgrounds.createCampground))
-
+        //.post(isLoggedIn,validateCampground ,catchAsync(campgrounds.createCampground))
+        .post(upload.single('image'),(req,res)=>{
+            res.send(req.body,req.files)
+        })
 
 
 router.get('/new',isLoggedIn,campgrounds.renderNewForm);
